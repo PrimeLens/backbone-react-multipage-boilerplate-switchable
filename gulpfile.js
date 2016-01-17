5cd9d979266519675babd56211a7a5033aae3088
PrimeLens <jhoedavis@gmail.com>
2016-01-16 23:56:39 -0500

    running tests with moving files

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,11 @@
 // http://stackoverflow.com/questions/33018779/using-gulp-without-global-gulp-edit-and-without-linking-to-the-bin-js-file
 // http://stackoverflow.com/questions/22115400/why-do-we-need-to-install-gulp-globally-and-locally
 
+// this gulp is set up to NOT use gulp serve. We want to use the express server provided instead as 
+// there will be proxy endpoints.
+// so in effect gulp, is no different to grunt in the task it performs here
+
+
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 
@@ -29,6 +34,7 @@ gulp.task('build', function(callback){
 // gulp.task('build', ['one', 'two', 'three']);
 gulp.task('one', function(){   
 	console.log('one');  
+	return gulp.src('./src/js/router/**/*.js').pipe(gulp.dest('./public/testing'));
 });
 gulp.task('two', function(){   
 	console.log('two');  
