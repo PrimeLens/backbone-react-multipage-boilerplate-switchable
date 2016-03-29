9440373f2cba6184faa6329c1c1b8a00490f959b
l <mr.thomas.s.kim@gmail.com>
2016-03-29 17:25:54 -0400

    AWS SDK and STS integration.

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ module.exports = function(){
 
 	var express = require('express');
 	var bodyParser = require('body-parser');
+	var aws = require('aws-sdk');
+	var sts = new aws.STS();
+	var keys = require('./keys'); 
 
 
 	var app = express();
@@ -15,6 +18,35 @@ module.exports = function(){
 
 	app.use(express.static(__dirname + '/public'));
 
+  /*
+  ** This is a sample endpoint to demonstrate the code
+  ** that is necessary to get an AWS STS token.
+  ** A keys.js file is needed in the root directory
+  ** to make this work.
+  ** The contents of the keys file should be...
+  ** var keys = {
+  **  accessKeyId: 'enter a real access key iD here',
+  **  secretAccessKey: 'enter a secret access key here'
+  ** };
+  */
+  // These are the credentials for the macmillan-front-end user.
+  aws.config.update({
+    accessKeyId: keys.accessKeyId,
+    secretAccessKey: keys.secretAccessKey
+  });
+
+  app.get('/token', function(req, res) {
+    var params = {
+      DurationSeconds: 3600
+    };
+    sts.getSessionToken(params, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ token: data }));
+      }
+    });
+  });
 
 	// alter response header so it doesn't say powered by expressJS
 	app.use(function(req, res, next){
@@ -25,4 +57,3 @@ module.exports = function(){
 	return app;
 
 }
-	
