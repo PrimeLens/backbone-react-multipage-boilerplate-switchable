0c231e1d9d508b5bf4adf83e8bc1d6cdccbc9d90
PrimeLens <jhoedavis@gmail.com>
2016-07-04 00:34:56 -0400

    straighten up code indent and formatting for gulpfile and karma.conf.js

--- gulpfile.js
+++ gulpfile.js
@@ -39,147 +39,147 @@ var paths = require('./gulpconfig/gulpconfig.js');
 
 
 gulp.task('default', function(){   
-	console.log('to use');
-	console.log('npm run build');
-	console.log('or');
-	console.log('npm run buildwatch');			  
+    console.log('to use');
+    console.log('npm run build');
+    console.log('or');
+    console.log('npm run buildwatch');			  
 });
 
 // build
 gulp.task('build', function(done){
-  runSequence(['1:partials', '2:jsx', '3:cssBundle'], ['4:jsBundle'], done);
+    runSequence(['1:partials', '2:jsx', '3:cssBundle'], ['4:jsBundle'], done);
 });
 
 gulp.task('1:partials', function(){   
-  return gulp.src(paths.partials)
-		.pipe(fc2json('htmlpartials.js', {extname:false, flat:true}))   // add  , {extname:false})) if they accept my pull request
-		.pipe(insert.prepend('window.htmlpartials = '))
-		.pipe(insert.append(';'))
-		.pipe(gulp.dest('./srcbase/htmlcompiled'));
+    return gulp.src(paths.partials)
+    	.pipe(fc2json('htmlpartials.js', {extname:false, flat:true}))   // add  , {extname:false})) if they accept my pull request
+    	.pipe(insert.prepend('window.htmlpartials = '))
+    	.pipe(insert.append(';'))
+    	.pipe(gulp.dest('./srcbase/htmlcompiled'));
 });
 gulp.task('2:jsx', function(){   
-  return gulp.src(paths.jsx)
+    return gulp.src(paths.jsx)
 		// insert header comment showing filename and tag it so its not deleted
 		.pipe(insert.transform(function(contents, file) {
 			var filename = file.path.replace(file.base,'');
 			var comment = '/*! ' + filename + ' */ \n';
 			return comment + contents;
 		}))
-    /*
-    ** Plumber provides custom error-handling.
-    ** 'Found an error' text is printed in red to make it easier to pinpoint errors in the output.
-    ** Also, there is no need to restart Gulp after finding an error.
-    */
-    .pipe(plumber({
-      errorHandler: function(error) {
-        gutil.log(
-          gutil.colors.cyan('Plumber') + gutil.colors.red(' found an error:\n'),
-          error.toString()
-        );
-        this.emit('end');
-      }
-    }))
-		// translate jsx
-    .pipe(babel({
-      presets: ['es2015', 'react']
-    }))
-    .pipe(plumber.stop())
-		// remove comments, cannot strip comments from jsx file as it crashes
-    .pipe(strip({ safe : true }))		
-    .pipe(removeEmptyLines())
-    .pipe(concat('jsxcompiled.js'))
-    .pipe(gulp.dest('./srcbase/jsxcompiled'));	
+        /*
+        ** Plumber provides custom error-handling.
+        ** 'Found an error' text is printed in red to make it easier to pinpoint errors in the output.
+        ** Also, there is no need to restart Gulp after finding an error.
+        */
+        .pipe(plumber({
+            errorHandler: function(error) {
+                gutil.log(
+                    gutil.colors.cyan('Plumber') + gutil.colors.red(' found an error:\n'),
+                    error.toString()
+                );
+                this.emit('end');
+            }
+        }))
+    	// translate jsx
+        .pipe(babel({
+            presets: ['es2015', 'react']
+        }))
+        .pipe(plumber.stop())
+    	// remove comments, cannot strip comments from jsx file as it crashes
+        .pipe(strip({ safe : true }))		
+        .pipe(removeEmptyLines())
+        .pipe(concat('jsxcompiled.js'))
+        .pipe(gulp.dest('./srcbase/jsxcompiled'));	
 });
 
 gulp.task('3:cssBundle', function(){   
-  return gulp.src(paths.sass)
-    .pipe(plumber({
-      errorHandler: function(error) {
-        gutil.log(
-          gutil.colors.cyan('Plumber') + gutil.colors.red(' found an error:\n'),
-          error.toString()
-        );
-        this.emit('end');
-      }
-    }))
+    return gulp.src(paths.sass)
+        .pipe(plumber({
+          errorHandler: function(error) {
+            gutil.log(
+              gutil.colors.cyan('Plumber') + gutil.colors.red(' found an error:\n'),
+              error.toString()
+            );
+            this.emit('end');
+          }
+        }))
 
-    .pipe(plumber.stop())
-    .pipe(autoprefixer())
-		// insert header comment showing filename and tag it so its not deleted
-		.pipe(insert.transform(function(contents, file) {
-      var filename = file.path.replace(file.base,'').replace('.css','.scss');
-			var comment = '/*! ' + filename + ' */ \n';
-			return comment + contents;
-		}))
-		// remove comments, cannot strip comments from jsx file as it crashes
-    .pipe(concat('start.scss'))
-    .pipe(sass({
-      // outputStyle: 'compressed',
-      outputStyle: 'nested',
-      sourceComments: 'map',
-      includePaths: []
-    }))
-    .pipe(stripcss())
-    .pipe(removeEmptyLines())
-    .pipe(addsrc.prepend('./src/css/bootstrap.min.css'))
-    .pipe(concat('start.css'))
-    .pipe(gulp.dest('./public/prod'));           
+        .pipe(plumber.stop())
+        .pipe(autoprefixer())
+            // insert header comment showing filename and tag it so its not deleted
+            .pipe(insert.transform(function(contents, file) {
+                var filename = file.path.replace(file.base,'').replace('.css','.scss');
+                var comment = '/*! ' + filename + ' */ \n';
+                return comment + contents;
+            }))
+            // remove comments, cannot strip comments from jsx file as it crashes
+        .pipe(concat('start.scss'))
+        .pipe(sass({
+            // outputStyle: 'compressed',
+            outputStyle: 'nested',
+            sourceComments: 'map',
+            includePaths: []
+        }))
+        .pipe(stripcss())
+        .pipe(removeEmptyLines())
+        .pipe(addsrc.prepend('./src/css/bootstrap.min.css'))
+        .pipe(concat('start.css'))
+        .pipe(gulp.dest('./public/prod'));           
 });
 
 gulp.task('4:jsBundle', function(){   
-  return gulp.src(paths.js)
-    // Remove 'use strict';
-    .pipe(replace(/('|")use strict\1;/g, ''))
-		// insert header comment showing filename and tag it so its not deleted
-		.pipe(insert.transform(function(contents, file) {
-			var filename = file.path.replace(file.base,'');
-			var comment = '/*! ' + filename + ' */ \n';
-			return comment + contents;
-		}))
-		// remove comments, cannot strip comments from jsx file as it crashes
-    .pipe(strip({ safe : true }))
-    .pipe(removeEmptyLines())
-    .pipe(addsrc.prepend(paths.thirdParty))
-    .pipe(concat('start.js'))
-    .pipe(gulp.dest('./public/prod')) ;   
+    return gulp.src(paths.js)
+        // Remove 'use strict';
+        .pipe(replace(/('|")use strict\1;/g, ''))
+        // insert header comment showing filename and tag it so its not deleted
+        .pipe(insert.transform(function(contents, file) {
+            var filename = file.path.replace(file.base,'');
+            var comment = '/*! ' + filename + ' */ \n';
+            return comment + contents;
+        }))
+        // remove comments, cannot strip comments from jsx file as it crashes
+        .pipe(strip({ safe : true }))
+        .pipe(removeEmptyLines())
+        .pipe(addsrc.prepend(paths.thirdParty))
+        .pipe(concat('start.js'))
+        .pipe(gulp.dest('./public/prod')) ;   
 });
 
 gulp.task('test', function(done) {
-  var karmaServer = new karma({
-    singleRun: true,
-    configFile: __dirname + '/karma.conf.js'
-  });
-  karmaServer.on('run_complete', function(browsers, results) {
-    done(results.error ? 'There are test failures' : null);
-  });
-  karmaServer.start();
+    var karmaServer = new karma({
+        singleRun: true,
+        configFile: __dirname + '/karma.conf.js'
+    });
+    karmaServer.on('run_complete', function(browsers, results) {
+        done(results.error ? 'There are test failures' : null);
+    });
+    karmaServer.start();
 });
 
 gulp.task('testwatch', function(done) {
-  new karma({
-    singleRun: false,
-    configFile: __dirname + '/karma.conf.js'
-  }, function(){ done(); }).start();
+    new karma({
+        singleRun: false,
+        configFile: __dirname + '/karma.conf.js'
+    }, function(){ done(); }).start();
 });
 
 gulp.task('buildtest', function(done) {
-  runSequence(['build'], ['test']);
+    runSequence(['build'], ['test']);
 });
 
 gulp.task('buildwatch', function(done) {
-  runSequence(['build']);
-  gulp.watch(paths.partials, ['1:partials']);
-  gulp.watch(paths.jsx, ['2:jsx']);
-  gulp.watch(paths.sass, ['3:cssBundle']);
-  gulp.watch(paths.js, ['4:jsBundle']);
-  gulp.watch(paths.unitTests, ['testwatch']);
+    runSequence(['build']);
+    gulp.watch(paths.partials, ['1:partials']);
+    gulp.watch(paths.jsx, ['2:jsx']);
+    gulp.watch(paths.sass, ['3:cssBundle']);
+    gulp.watch(paths.js, ['4:jsBundle']);
+    gulp.watch(paths.unitTests, ['testwatch']);
 });
 
 function string_src(filename, string) {
-  var src = require('stream').Readable({ objectMode: true })
-  src._read = function () {
-    this.push(new gutil.File({ cwd: "", base: "", path: filename, contents: new Buffer(string) }))
-    this.push(null)
-  }
-  return src
+    var src = require('stream').Readable({ objectMode: true })
+    src._read = function () {
+        this.push(new gutil.File({ cwd: "", base: "", path: filename, contents: new Buffer(string) }));
+        this.push(null);
+    }
+    return src
 }
--- karma.conf.js
+++ karma.conf.js
@@ -2,74 +2,66 @@
 
 module.exports = function(config) {
 
-  // './src/js/lib/**/*.js',
-  var specFiles = './!(node_modules)/**/*.spec.js',
-      platformSpecFile = './platform.spec.js',
-      filesToTest = [
-      './src/js/lib/md5.js',
-      './public/prod/start.js',
-      platformSpecFile,
-      specFiles
-    ]; 
+    // './src/js/lib/**/*.js',
+    var specFiles = './!(node_modules)/**/*.spec.js',
+        platformSpecFile = './platform.spec.js',
+        filesToTest = [
+            './src/js/lib/md5.js',
+            './public/prod/start.js',
+            platformSpecFile,
+            specFiles
+        ]; 
 
-  config.set({
+    config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
-
-    // list of files / patterns to load in the browser
-    files: filesToTest,
-
-    // list of files to exclude
-    exclude: [
-    ],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+        // list of files / patterns to load in the browser
+        files: filesToTest,
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+        // list of files to exclude
+        exclude: [],
 
 
-    // web server port
-    port: 9876,
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {},
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress'],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // web server port
+        port: 9876,
 
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['PhantomJS'],
 
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true,
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: Infinity
+    })
 }
