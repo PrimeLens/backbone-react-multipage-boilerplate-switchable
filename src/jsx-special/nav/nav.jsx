bff2ecaba232d26661e79705367e491d2c870ed3
Justin Evans <jurevans@gmail.com>
2016-10-19 11:50:39 -0400

    Restoring affected files :/

--- src/jsx-special/nav/nav.jsx
+++ src/jsx-special/nav/nav.jsx
@@ -6,7 +6,7 @@ rc.nav = React.createClass({
     },
 	componentDidMount : function(){
 		var self= this;
-        // unbind before binding in case component unmounts/remounts, optionally use componentWillUnmount	
+        // unbind before binding in case component unmounts/remounts, optionally use componentWillUnmount
 	    grandCentral.off('pagechange').on('pagechange', function(data){
 			self.setState({
 				currentPage: data.currentPage
@@ -23,8 +23,22 @@ rc.nav = React.createClass({
     render:function(){
         return (
 <div>
-															</div>
+	<a className={this.getClassNameWithActive('home')} href="#">Home</a>
+	<a className={this.getClassNameWithActive('exmachina')} href="#/exmachina">Ex Machina</a>
+	<a className={this.getClassNameWithActive('gameofthrones')} href="#/gameofthrones">Game Of Thrones</a>
+	<a className={this.getClassNameWithActive('trueblood')} href="#/trueblood">True Blood</a>
+	<a className={this.getClassNameWithActive('dexter')} href="#/dexter">Dexter</a>
+	<a className={this.getClassNameWithActive('walkingdead')} href="#/walkingdead">Walking Dead</a>
+	<a className={this.getClassNameWithActive('hungergames')} href="#/hungergames">Hunger Games</a>
+	<a className={this.getClassNameWithActive('hannibal')} href="#/hannibal">Hannibal</a>
+	<a className={this.getClassNameWithActive('breakingbad')} href="#/breakingbad">Breaking Bad</a>
+	<a className={this.getClassNameWithActive('firefly')} href="#/firefly">Firefly</a>
+	<a className={this.getClassNameWithActive('madmax')} href="#/madmax">Mad Max</a>
+	<a className={this.getClassNameWithActive('inception')} href="#/inception">Inception</a>
+	<a className={this.getClassNameWithActive('anime')} href="#/anime">Anime</a>
+	<a className={this.getClassNameWithActive('jessicajones')} href="#/jessicajones">Jessica Jones</a>
+	<a className={this.getClassNameWithActive('bladerunner')} href="#/bladerunner">Blade Runner</a>
+</div>
         );
     }
 });
-	
